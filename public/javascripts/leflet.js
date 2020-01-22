756920083bb1ce85db8ab3d243537269b23bd90b
Vladimir Mihaylov <zombotv82@gmail.com>
2020-01-22 11:21:41 +0700

    ready to use map

--- public/javascripts/leflet.js
+++ public/javascripts/leflet.js
@@ -1,19 +1,19 @@
 var mymap = L.map('mapid').setView([50.1188 , 8.6843], 4);
-
-var coords = coord.split(',');
-var marker
-coords = coords.map( item => item.replace(' ', ','));
-coords = coords.map( item => {
-    let arr = item.split(',');
-    arr = arr.map( item => parseFloat(item))
-    marker = L.marker(arr).addTo(mymap);
-    marker.bindPopup(`ip:`).openPopup();
-})
-
-
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibGFzdGhlcm9pbiIsImEiOiJjazVsem9sa3cwZ3JxM2RydzNpZTViajZwIn0.iX3QFFLWX40Vo7FB-IjxrQ', {
     attribution: '&copy; <a href="http://openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    maxZoom: 8,
+    maxZoom: 9,
+    minZoom: 2,
     id: 'mapbox/streets-v11',
     accessToken: 'your.mapbox.access.token'
 }).addTo(mymap);
+coord = coord.split(',');
+var markers = L.markerClusterGroup();
+coord.forEach(element => {
+    let res = element.split(' ');
+    var a = [res[0],res[1]];
+    var marker = L.marker(new L.LatLng(a[0],a[1]), {title:'dsa'});
+    markers.addLayer(marker);
+
+});
+mymap.addLayer(markers);
+
